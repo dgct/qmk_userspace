e4c444b90f1c850ae8887700d0cd71ba0e07610b
dgct <d@tree.casa>
2025-01-03 16:11:55 -0800

    3rd select word

--- keyboards/bastardkb/dilemma/4x6_4/keymaps/my_keymap/keymap.c
+++ keyboards/bastardkb/dilemma/4x6_4/keymaps/my_keymap/keymap.c
@@ -1,20 +1,22 @@
 /**************************************************************************************
- *  Existing GPL License Header from your keymap (unchanged)
+ * GPL License Header (from your keymap code)
  **************************************************************************************/
 /**
  * Copyright 2021 Charly Delay <charly@codesink.dev> (@0xcharly)
  * Copyright 2023 casuanoob <casuanoob@hotmail.com> (@casuanoob)
  *
- * This program is free software: you can redistribute it and/or modify it under
- * the terms of the GNU General Public License as published by the Free Software
- * Foundation, either version 2 of the License, or (at your option) any later version.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
- * PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License along with
- * this program. If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include QMK_KEYBOARD_H
@@ -37,6 +39,17 @@ void keyboard_post_init_user(void) {
     defer_exec(100, custom_os_settings, NULL);
 }
 #endif
+
+/**************************************************************************************
+ * Custom Keycode Definition
+ *
+ * `SELWORD` is a custom keycode that triggers the “Select Word” functionality.
+ * Make sure this value is also exposed in your VIA JSON if you want to assign it in VIA.
+ **************************************************************************************/
+enum custom_keycodes {
+    SELWORD = SAFE_RANGE, // Start custom keycodes at SAFE_RANGE
+    // add more custom keycodes here if you like
+};
 
 enum dilemma_keymap_layers {
     LAYER_BASE = 0,
@@ -63,35 +76,35 @@ enum dilemma_keymap_layers {
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [LAYER_BASE] = LAYOUT(
-        KC_ESC,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,       KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_MINS,
-        KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,       KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_BSLS,
-       KC_LSFT,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,       KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,
-       KC_LCTL,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,       KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_LGUI,
-                         KC_LALT, KC_BSPC,  KC_SPC,   LOWER,      RAISE,  KC_ENT, KC_DEL,  KC_MUTE
+        KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,       KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,       KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
+       KC_LSFT,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,       KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+       KC_LCTL,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,       KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_LGUI,
+                             KC_LALT, KC_BSPC, KC_SPC, LOWER,     RAISE,   KC_ENT,  KC_DEL,  KC_MUTE
   ),
 
   [LAYER_QWERTY] = LAYOUT(
-        KC_ESC,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,       KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_MINS,
-        KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,       KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_BSLS,
-       KC_LSFT,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,       KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,
-       KC_LCTL,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,       KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_LGUI,
-                         KC_LALT, KC_BSPC,  KC_SPC,   LOWER,      RAISE,  KC_ENT, KC_DEL,  KC_MUTE
+        KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,       KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,       KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
+       KC_LSFT,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,       KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+       KC_LCTL,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,       KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_LGUI,
+                             KC_LALT, KC_BSPC, KC_SPC, LOWER,     RAISE,   KC_ENT,  KC_DEL,  KC_MUTE
   ),
 
   [LAYER_LOWER] = LAYOUT(
-       KC_TILD, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,    KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_UNDS,
-       RGB_MOD, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    KC_LBRC,   KC_P7,   KC_P8,   KC_P9, KC_RBRC, XXXXXXX,
-       RGB_TOG, KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, XXXXXXX,    KC_PPLS,   KC_P4,   KC_P5,   KC_P6, KC_PMNS, KC_PEQL,
-      RGB_RMOD, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    KC_PAST,   KC_P1,   KC_P2,   KC_P3, KC_PSLS, KC_PDOT,
-                         XXXXXXX, XXXXXXX, XXXXXXX, _______,      POINT, _______, XXXXXXX,   KC_P0
+       KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,   KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_UNDS,
+       RGB_MOD, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,   KC_LBRC, KC_P7,   KC_P8,   KC_P9,   KC_RBRC, XXXXXXX,
+       RGB_TOG, KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, XXXXXXX,   KC_PPLS, KC_P4,   KC_P5,   KC_P6,   KC_PMNS, KC_PEQL,
+      RGB_RMOD, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,   KC_PAST, KC_P1,   KC_P2,   KC_P3,   KC_PSLS, KC_PDOT,
+                               XXXXXXX, XXXXXXX, XXXXXXX,  _______, POINT,  _______, XXXXXXX, KC_P0
   ),
 
   [LAYER_RAISE] = LAYOUT(
-        KC_F12,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,      KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,
+        KC_F12,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,      KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
        KC_MNXT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_VOLU,
-       KC_MPLY, KC_LEFT,   KC_UP, KC_DOWN, KC_RGHT, XXXXXXX,    XXXXXXX, KC_RSFT, KC_RCTL, KC_RALT, KC_RGUI, KC_MUTE,
-       KC_MPRV, KC_HOME, KC_PGUP, KC_PGDN,  KC_END, XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_VOLD,
-                         XXXXXXX, _______, _______,   POINT,    _______, XXXXXXX, XXXXXXX, XXXXXXX
+       KC_MPLY, KC_LEFT, KC_UP,   KC_DOWN, KC_RGHT, XXXXXXX,    XXXXXXX, KC_RSFT, KC_RCTL, KC_RALT, KC_RGUI, KC_MUTE,
+       KC_MPRV, KC_HOME, KC_PGUP, KC_PGDN, KC_END,  XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_VOLD,
+                               XXXXXXX, _______, _______, POINT, _______, XXXXXXX, XXXXXXX, XXXXXXX
   ),
 
   [LAYER_POINTER] = LAYOUT(
@@ -99,7 +112,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, DPI_MOD, S_D_MOD,    S_D_MOD, DPI_MOD, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
        XXXXXXX, KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, XXXXXXX,    XXXXXXX, KC_RSFT, KC_RCTL, KC_RALT, KC_RGUI, XXXXXXX,
        XXXXXXX, _______, DRGSCRL, SNIPING, EE_CLR,  QK_BOOT,    QK_BOOT, EE_CLR,  SNIPING, DRGSCRL, _______, XXXXXXX,
-                         XXXXXXX, KC_BTN2, KC_BTN1, KC_BTN3,    KC_BTN3, KC_BTN1, KC_BTN2, XXXXXXX
+                               XXXXXXX, KC_BTN2, KC_BTN1, KC_BTN3, KC_BTN3, KC_BTN1, KC_BTN2, XXXXXXX
   ),
 };
 // clang-format on
@@ -136,7 +149,7 @@ const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
 #endif // ENCODER_MAP_ENABLE
 
 /**************************************************************************************
- *  Add the Google Apache-licensed Select Word code below, adjusted for OS detection
+ * Apache License Header (Google’s "Select Word" implementation)
  **************************************************************************************/
 // Copyright 2021-2023 Google LLC
 //
@@ -160,11 +173,12 @@ const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
  * <https://getreuer.info/posts/keyboards/select-word>
  */
 
-// If you extracted select_word.h somewhere, include it here:
+// If you extracted select_word.h somewhere, include it here
 // #include "select_word.h"
 
+// Idle timeout in ms
 #ifndef SELECT_WORD_TIMEOUT
-#    define SELECT_WORD_TIMEOUT 5000 // Example default timeout (ms)
+#    define SELECT_WORD_TIMEOUT 5000
 #endif
 
 // clang-format off
@@ -190,14 +204,9 @@ void select_word_task(void) {
 #endif // SELECT_WORD_TIMEOUT > 0
 
 /**
- * Example usage in your process_record_user():
+ * @brief OS-detection-based logic to replace #define MAC_HOTKEYS
  *
- *   bool process_record_user(uint16_t keycode, keyrecord_t* record) {
- *     if (!process_select_word(keycode, record, SELWORD)) {
- *         return false;
- *     }
- *     return true;
- *   }
+ * The custom keycode `SELWORD` is used to trigger this logic.
  */
 bool process_select_word(uint16_t keycode, keyrecord_t *record, uint16_t sel_keycode) {
     // Don’t interfere with shift-taps.
@@ -206,22 +215,21 @@ bool process_select_word(uint16_t keycode, keyrecord_t *record, uint16_t sel_key
     }
 
 #if SELECT_WORD_TIMEOUT > 0
-    // Reset idle timer every time we see a relevant key event.
+    // Reset idle timer every time we see a relevant key event
     idle_timer = record->event.time + SELECT_WORD_TIMEOUT;
 #endif
 
-    // If the key pressed is our "select word" key:
+    // Only act if this is our “Select Word” keycode
     if (keycode == sel_keycode && record->event.pressed) {
         const uint8_t mods = get_mods();
-
 #ifndef NO_ACTION_ONESHOT
         const bool shifted = (mods | get_oneshot_mods()) & MOD_MASK_SHIFT;
         clear_oneshot_mods();
 #else
-        const bool shifted = mods & MOD_MASK_SHIFT;
-#endif
+        const bool shifted = (mods & MOD_MASK_SHIFT);
+#endif // NO_ACTION_ONESHOT
 
-        // Detect OS for appropriate key combos
+        // OS detection
 #ifdef OS_DETECTION_ENABLE
         os_variant_t host     = detected_host_os();
         bool         is_macos = (host == OS_MACOS || host == OS_IOS);
@@ -229,19 +237,17 @@ bool process_select_word(uint16_t keycode, keyrecord_t *record, uint16_t sel_key
         bool is_macos = false; // fallback if OS detection is off
 #endif
 
+        // --- Select Word (no shift) ---
         if (!shifted) {
-            // ----- Select word -----
-            // On macOS, hold Left Alt (Option).
-            // Otherwise, hold Left Ctrl.
             if (is_macos) {
-                set_mods(MOD_BIT(KC_LALT));
+                set_mods(MOD_BIT(KC_LALT)); // macOS uses Alt (Option) for word jumps
             } else {
-                set_mods(MOD_BIT(KC_LCTL));
+                set_mods(MOD_BIT(KC_LCTL)); // Windows/Linux use Ctrl
             }
 
             if (state == STATE_NONE) {
-                // On first use, tap Ctrl+Right then Ctrl+Left
-                // (or Alt+Right then Alt+Left on macOS)
+                // On first use, ensure cursor is at start of current word:
+                // tap Ctrl+Right then Ctrl+Left (or Alt+Right then Alt+Left)
                 send_keyboard_report();
                 tap_code(KC_RGHT);
                 tap_code(KC_LEFT);
@@ -249,45 +255,48 @@ bool process_select_word(uint16_t keycode, keyrecord_t *record, uint16_t sel_key
             register_mods(MOD_BIT(KC_LSFT));
             register_code(KC_RGHT);
             state = STATE_WORD;
-
-        } else {
-            // ----- Select line -----
+        }
+        // --- Select Line (with shift) ---
+        else {
             if (state == STATE_NONE) {
                 if (is_macos) {
-                    // Tap GUI(Command)+Left, then SHIFT+GUI(Command)+Right
-                    // 1) Press Command
-                    set_mods(MOD_BIT(KC_LGUI));
+                    // On macOS, line selection can be done with:
+                    // 1) GUI(Command) + Left
+                    // 2) SHIFT + GUI(Command) + Right
+                    set_mods(MOD_BIT(KC_LGUI)); // Press Command
                     send_keyboard_report();
                     tap_code(KC_LEFT);
 
-                    // 2) Press Shift+Command+Right
+                    // Then press Shift+Command+Right
                     register_mods(MOD_BIT(KC_LSFT));
                     tap_code(KC_RGHT);
 
-                    // Restore previous mods
+                    // Restore original mods
                     set_mods(mods);
                 } else {
-                    // Tap Home, then SHIFT+End
+                    // On Windows, line selection can be done with:
+                    // 1) Home
+                    // 2) SHIFT + End
                     clear_mods();
                     send_keyboard_report();
                     tap_code(KC_HOME);
                     register_mods(MOD_BIT(KC_LSFT));
                     tap_code(KC_END);
 
-                    // Restore previous mods
+                    // Restore original mods
                     set_mods(mods);
                 }
                 state = STATE_FIRST_LINE;
             } else {
-                // Already selecting a line, go down one more line
+                // Already started line selection -> continue selecting next line
                 register_code(KC_DOWN);
                 state = STATE_LINE;
             }
         }
-        return false;
+        return false; // We've handled this press
     }
 
-    // If select_word is being released, or any other key pressed
+    // If we get here, either the key was released or a different key was pressed.
     switch (state) {
         case STATE_WORD:
             unregister_code(KC_RGHT);
@@ -302,7 +311,6 @@ bool process_select_word(uint16_t keycode, keyrecord_t *record, uint16_t sel_key
                 }
             }
 #else
-            // fallback
             unregister_mods(MOD_BIT(KC_LSFT) | MOD_BIT(KC_LCTL));
 #endif
             state = STATE_SELECTED;
@@ -318,18 +326,35 @@ bool process_select_word(uint16_t keycode, keyrecord_t *record, uint16_t sel_key
             break;
 
         case STATE_SELECTED:
-            // If user hits Esc, we move cursor to the right
-            // to break the selection
+            // If user presses Esc while selected, break the selection
             if (keycode == KC_ESC) {
                 tap_code(KC_RGHT);
                 state = STATE_NONE;
                 return false;
             }
-            // fall-through
+            // fallthrough
         default:
             state = STATE_NONE;
             break;
     }
 
     return true;
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t record) {
+    if (!process_select_word(keycode, record, SELWORD)) {
+        return false; // "Select Word" handled the key
+    }
+
+    switch (keycode) {
+        case SELWORD:
+            // Usually nothing here because process_select_word() does all the work
+            break;
+            // ... other custom keycodes ...
+    }
+    return true;
+}
+
+void matrix_scan_user(void) {
+    select_word_task();
 }
