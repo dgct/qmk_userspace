4fc06a5d51d20dd2707400aaa222ef6e08f86611
dgct <d@tree.casa>
2025-01-26 00:05:32 -0800

    sm_td try 3

--- keyboards/bastardkb/dilemma/4x6_4/keymaps/my_keymap/keymap.c
+++ keyboards/bastardkb/dilemma/4x6_4/keymaps/my_keymap/keymap.c
@@ -20,9 +20,9 @@
 enum custom_keycodes {
     SMTD_KEYCODES_BEGIN = SAFE_RANGE,
     CKC_A, // reads as C(ustom) + KC_A, but you may give any name here
+    CKC_R,
     CKC_S,
-    CKC_D,
-    CKC_F,
+    CKC_T,
     CKC_N,
     CKC_E,
     CKC_I,
@@ -76,11 +76,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // ╭──────────────────────────────────────────────────────╮ ╭──────────────────────────────────────────────────────╮
         KC_ESC,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,       KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_MINS,
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
-        KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,       KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_BSLS,
+        KC_TAB,    KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,       KC_J,    KC_L,    KC_U,    KC_Y, KC_SCLN, KC_BSLS,
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
-       KC_LSFT,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,       KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,
+       KC_LSFT,   CKC_A,   CKC_R,   CKC_S,   CKC_T,    KC_G,       KC_M,   CKC_N,   CKC_E,   CKC_I,   CKC_O, KC_QUOT,
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
-       KC_LCTL,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,       KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_LGUI,
+       KC_LCTL,    KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,       KC_K,    KC_H, KC_COMM,  KC_DOT, KC_SLSH, KC_RALT,
   // ╰──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────╯
                          KC_LALT, KC_BSPC,  KC_SPC,   LOWER,      RAISE,  KC_ENT, KC_DEL,  KC_MUTE
   //                    ╰───────────────────────────────────╯ ╰───────────────────────────────────╯
@@ -167,9 +167,9 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     void on_smtd_action(uint16_t keycode, smtd_action action, uint8_t tap_count) {
         switch (keycode) {
             SMTD_MT(CKC_A, KC_A, KC_LEFT_GUI)
-            SMTD_MT(CKC_S, KC_S, KC_LEFT_ALT)
-            SMTD_MT(CKC_D, KC_D, KC_LEFT_CTRL)
-            SMTD_MT(CKC_F, KC_F, KC_LSFT)
+            SMTD_MT(CKC_R, KC_R, KC_LEFT_ALT)
+            SMTD_MT(CKC_S, KC_S, KC_LEFT_CTRL)
+            SMTD_MT(CKC_T, KC_T, KC_LSFT)
             SMTD_MT(CKC_N, KC_N, KC_RSFT)
             SMTD_MT(CKC_E, KC_E, KC_RIGHT_CTRL)
             SMTD_MT(CKC_I, KC_I, KC_RIGHT_ALT)
